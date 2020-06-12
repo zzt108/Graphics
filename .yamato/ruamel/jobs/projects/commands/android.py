ba6ed05793afb7e21bde7eae69f7e5a16dbabbb7
tomzig16 <tzigmantavicius@gmail.com>
2020-06-12 16:21:00 +0300

    Add OpenGLES2 configuration

--- .yamato/ruamel/jobs/projects/commands/android.py
+++ .yamato/ruamel/jobs/projects/commands/android.py
@@ -35,6 +35,8 @@ def cmd_standalone_build(project_folder, platform, api, test_platform_args):
         ])
     if api.lower() =='vulkan':
         base.append(f'utr\\utr --suite=playmode --platform=Android --testproject={TEST_PROJECTS_DIR}\{project_folder} --extra-editor-arg="-executemethod" --extra-editor-arg="SetupProject.ApplySettings" --extra-editor-arg="vulkan" --editor-location=WindowsEditor --artifacts_path={PATH_TEST_RESULTS} --player-save-path={PATH_PLAYERS} --scripting-backend=il2cpp --timeout=1200 --build-only')
+    elif api.lower() == 'opengles2':
+        base.append(f'utr\\utr --suite=playmode --platform=Android --testproject={TEST_PROJECTS_DIR}\{project_folder} --extra-editor-arg="-executemethod" --extra-editor-arg="SetupProject.ApplySettings" --extra-editor-arg="opengles2" --editor-location=WindowsEditor --artifacts_path={PATH_TEST_RESULTS} --player-save-path={PATH_PLAYERS} --scripting-backend=il2cpp --timeout=1200 --build-only')
     else:
         base.append(f'utr\\utr --suite=playmode --platform=Android --testproject={TEST_PROJECTS_DIR}\{project_folder} --editor-location=WindowsEditor --artifacts_path={PATH_TEST_RESULTS} --player-save-path={PATH_PLAYERS} --scripting-backend=il2cpp --timeout=1200 --build-only')
     return base
