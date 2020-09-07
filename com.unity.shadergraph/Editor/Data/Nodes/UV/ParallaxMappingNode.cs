25d3e6741389501ebdb58821e19dbd3622c1055c
tomzig16 <tzigmantavicius@gmail.com>
2020-09-07 16:16:36 +0300

    Fix ParallaxMapping shader compilation on GLES2

--- com.unity.shadergraph/Editor/Data/Nodes/UV/ParallaxMappingNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/UV/ParallaxMappingNode.cs
@@ -78,7 +78,7 @@ namespace UnityEditor.ShaderGraph
             var amplitude = GetSlotValue(kAmplitudeSlotId, generationMode);
             var uvs = GetSlotValue(kUVsSlotId, generationMode);
 
-            sb.AppendLines(String.Format(@"$precision2 {5} = {4} + ParallaxMapping({0}, {1}, IN.{2}, {3} * 0.01, {4});",
+            sb.AppendLines(String.Format(@"$precision2 {5} = {4} + ParallaxMapping(TEXTURE2D_ARGS({0}, {1}), IN.{2}, {3} * 0.01, {4});",
                 heightmap,
                 edgesSampler.Any() ? GetSlotValue(kHeightmapSamplerSlotId, generationMode) : "sampler" + heightmap,
                 CoordinateSpace.Tangent.ToVariableName(InterpolatorType.ViewDirection),
